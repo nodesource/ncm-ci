3479a00e17a94bc72d2a413c3dab99483645578c
Julian Gruber <julian@juliangruber.com>
2018-10-25 10:14:27 -0400

    Use @juliangruber's suggestion for improving `fetch()` timeout💚
    Co-Authored-By: bnb <accounts@bnb.im>

--- bin/ncm-ci.js
+++ bin/ncm-ci.js
@@ -9,7 +9,9 @@ const fetch = require('node-fetch')
 
 // Setting up a timeout to be used with fetch() – will use the NCM_TIMEOUT environment
 // variable or default to 2 minutes (120 seconds / 120000 milliseconds)
-let NCM_TIMEOUT = process.env.NCM_TIMEOUT ? process.env.NCM_TIMEOUT : 120000
+const timeout = process.env.NCM_TIMEOUT
+  ? Number(process.env.NCM_TIMEOUT) * 1000
+  : 120000
 
 if (!process.env.NCM_TOKEN) {
   console.error(`
